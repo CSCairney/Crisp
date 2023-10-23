f5b88b645182c77b36d64502c1fdeaed9627df09
Charles.Cairney <charles.cairney@ecs.co.uk>
2023-10-23 12:03:41 +0100

    Changed auth to redux action

---
+++ Crisp/src/modules/login/store/actions/login.ts
@@ -0,0 +1,20 @@
+import { ActionWithThunk } from "~/modules/common/types/store";
+import { userLogin } from "../../types/users";
+import { authUser } from "..";
+import { toast } from "sonner";
+
+export function loginUser( userInfo: userLogin): ActionWithThunk {
+    return (dispatch, getState) => {
+        try {
+            if (getState().userState.users.some((user) => user.username === userInfo.username && user.password === userInfo.password)) {
+                dispatch(authUser(userInfo));
+                toast.success('Login successful');
+            } else {
+                toast.error("Invalid username or password");
+            }
+        } catch (e) {
+            toast.error("Error logging in user");
+            throw new Error("Error logging in user");
+        }
+    }
+}

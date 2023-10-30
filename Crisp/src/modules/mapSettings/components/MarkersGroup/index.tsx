c48452f5232d6e416c1f886ff7a3d03fa51d9ed2
Charles.Cairney <charles.cairney@ecs.co.uk>
2023-10-30 20:34:13 +0000

    Moved layers retrival to parent

--- Crisp/src/modules/mapSettings/components/MarkersGroup/index.tsx
+++ Crisp/src/modules/mapSettings/components/MarkersGroup/index.tsx
@@ -1,14 +1,16 @@
 import React from "react";
-import { selectMarkerLayers } from "../../store/selectors/markers";
-import { useAppSelector } from "../../../../store";
 import { Marker, Popup } from "react-leaflet";
 import "./styles.scss";
 import { LatLngExpression } from "leaflet";
+import { markerData } from "../../types/map";
 
-const MarkersGroup: React.FC = () => {
-    const markerLayers = useAppSelector(selectMarkerLayers);
-    if (markerLayers) {
-        markerLayers.flatMap((markerLayer) => {
+type MarkersGroupProps = {
+    layers: markerData[];
+};
+
+const MarkersGroup: React.FC<MarkersGroupProps> = ({layers}) => {
+    if (layers) {
+        layers.flatMap((markerLayer) => {
             return markerLayer.details.map((marker) => {
                 const coordinate: LatLngExpression = [marker.coordinate.lat, marker.coordinate.long];
                 return (

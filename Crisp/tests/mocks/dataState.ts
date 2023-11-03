9c225eeb18cc0a0a7ebc9e2c085eb684db3ba890
Charles.Cairney <charles.cairney@ecs.co.uk>
2023-11-03 22:05:01 +0000

    mock state updated

---
+++ Crisp/tests/mocks/dataState.ts
@@ -0,0 +1,9 @@
+import { dataState } from "../../src/modules/dataSelector/types/data";
+
+export const dataMockState: dataState = {
+    isLoading: false,
+    error: "",
+    dataLayers: [],
+    tempSelectedLayers: [],
+    selectedLayers: [],
+    };
--- Crisp/tests/mocks/index.ts
+++ Crisp/tests/mocks/index.ts
@@ -3,10 +3,12 @@ import { teamMockState } from "./teamState";
 import { userMockState } from "./userState";
 import { mapMockState } from "./mapState";
 import { overlayMockState } from "./overlayState";
+import { dataMockState } from "./dataState";
 
 export const mockStoreData: RootState = {
     teamState: teamMockState,
     userState: userMockState,
     mapState: mapMockState,
-    overlayState: overlayMockState
+    overlayState: overlayMockState,
+    dataState: dataMockState
 };

256191d86b56d50221ae6171325c087ba67a1732
Charles Cairney <charlescairney@yahoo.co.uk>
2023-11-11 13:13:24 +0000

    Contact page BG setup

--- Crisp/src/modules/contacts/components/index.tsx
+++ Crisp/src/modules/contacts/components/index.tsx
@@ -1,14 +1,19 @@
 import React from "react";
+import ContactBackground from "../../../assets/contact/background.jpeg";
+import "./styles.scss";
 
 type ContactProps = {
-    contactId: string
+    contactId?: string
 }
 
 const Contacts: React.FC<ContactProps> = () => {
     return (
-        <>
-        <h1> this is shit </h1>
-        </>
+    <div className="contact">
+        <div className="contact__overlay-wrapper">
+            <div className="contact__sidebar-container"></div>
+            <img className="contact__background" src={ContactBackground} alt="contact us background"/>
+        </div>
+    </div>
     )
 }
 

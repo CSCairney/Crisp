41951fcdb31034f6e825da0236319d6674185d71
Charles Cairney <charlescairney@yahoo.co.uk>
2023-11-11 11:45:42 +0000

    folder structure

---
+++ Crisp/src/modules/contacts/components/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type ContactProps = {
+    contactId: string
+}
+
+const Contacts: React.FC<ContactProps> = () => {
+    return (
+        <>
+        <h1> this is shit </h1>
+        </>
+    )
+}
+
+export default Contacts;

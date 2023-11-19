3b2553d34e6380c2b0066667bd6edbeba99cbc9e
CSCairney <charlescairney@yahoo.co.uk>
2023-11-19 23:37:19 +0000

    Temp fix

--- Crisp/src/pages/Contact/index.tsx
+++ Crisp/src/pages/Contact/index.tsx
@@ -1,11 +1,13 @@
+import Footer from "../../modules/common/components/Footer";
 import Contacts from "../../modules/contacts/components/index";
 import "./styles.scss";
 
 const Contact = () => {
     return (
-        <>
+        <div className="contact-page">
             <Contacts />
-        </>
+            <Footer />
+        </div>
     )
 }
 

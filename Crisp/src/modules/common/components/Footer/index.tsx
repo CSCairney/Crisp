7390ff98cf18ef8926eb94f14fca9b333121c5f8
CSCairney <charlescairney@yahoo.co.uk>
2023-11-19 23:08:22 +0000

    footer width

--- Crisp/src/modules/common/components/Footer/index.tsx
+++ Crisp/src/modules/common/components/Footer/index.tsx
@@ -10,39 +10,64 @@ import { FaFacebook } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 import { TbBackslash } from "react-icons/tb";
 
-
 const Footer: React.FC = () => {
-    const socialMediaHandler = (socialMedia: string) => {
-        window.open(`${socialMedia}`, "_blank");
-    }
-    
+  const socialMediaHandler = (socialMedia: string) => {
+    window.open(`${socialMedia}`, "_blank");
+  };
 
   return (
     <footer className="footer">
-      <section className="footer__social-media">
-        <FaFacebook className="footer__social-media-icon" onClick={() => (socialMediaHandler("https://facebook.com"))} />
-        <TbBackslash className="footer__social-media-icon-separator" />
-        <FaInstagram className="footer__social-media-icon" onClick={() => (socialMediaHandler("https://instagram.com"))} />
-        <TbBackslash className="footer__social-media-icon-separator" />
-        <FaTwitter className="footer__social-media-icon" onClick={() => (socialMediaHandler("https://twitter.com"))} />
-        <TbBackslash className="footer__social-media-icon-separator" />
-        <FaLinkedin className="footer__social-media-icon" onClick={() => (socialMediaHandler("https://www.linkedin.com/in/charlescairney/"))} />
-        <TbBackslash className="footer__social-media-icon-separator" />
-        <FaGithub className="footer__social-media-icon" onClick={() => (socialMediaHandler("https://github.com/CSCairney"))} />
-      </section>
-      <section className="footer__interaction">
-        <div className="footer__responsive-pages">
-            <Link to={MainRoutes.HOME} className='footer__pages-link' >Home</Link>
-            <Link to={MainRoutes.LOGIN} className='footer__pages-link' >Login</Link>
-        </div>
-        <div className="footer__logo-container">
-            <img src={logo} alt="Crisp Logo" className="footer__logo"/>
-        </div>
-        <div className="footer__contact-pages">
-            <Link to={SubRoutes.PRODUCTS} className='footer__pages-link' >{SubRoutes.CONTACT}</Link>
-            <Link to={SubRoutes.PRODUCTS} className='footer__pages-link' >{SubRoutes.ABOUT}</Link>
-        </div>
-      </section>
+      <div className="footer__body">
+        <section className="footer__social-media">
+          <FaFacebook
+            className="footer__social-media-icon"
+            onClick={() => socialMediaHandler("https://facebook.com")}
+          />
+          <TbBackslash className="footer__social-media-icon-separator" />
+          <FaInstagram
+            className="footer__social-media-icon"
+            onClick={() => socialMediaHandler("https://instagram.com")}
+          />
+          <TbBackslash className="footer__social-media-icon-separator" />
+          <FaTwitter
+            className="footer__social-media-icon"
+            onClick={() => socialMediaHandler("https://twitter.com")}
+          />
+          <TbBackslash className="footer__social-media-icon-separator" />
+          <FaLinkedin
+            className="footer__social-media-icon"
+            onClick={() =>
+              socialMediaHandler("https://www.linkedin.com/in/charlescairney/")
+            }
+          />
+          <TbBackslash className="footer__social-media-icon-separator" />
+          <FaGithub
+            className="footer__social-media-icon"
+            onClick={() => socialMediaHandler("https://github.com/CSCairney")}
+          />
+        </section>
+        <section className="footer__interaction">
+          <div className="footer__responsive-pages">
+            <Link to={MainRoutes.HOME} className="footer__pages-link">
+              Home
+            </Link>
+            <Link to={MainRoutes.LOGIN} className="footer__pages-link">
+              Login
+            </Link>
+          </div>
+          <div className="footer__logo-container">
+            <img src={logo} alt="Crisp Logo" className="footer__logo" />
+          </div>
+          <div className="footer__contact-pages">
+            <Link to={SubRoutes.PRODUCTS} className="footer__pages-link">
+              {SubRoutes.CONTACT}
+            </Link>
+            <Link to={SubRoutes.PRODUCTS} className="footer__pages-link">
+              {SubRoutes.ABOUT}
+            </Link>
+          </div>
+        </section>
+      </div>
     </footer>
   );
 };

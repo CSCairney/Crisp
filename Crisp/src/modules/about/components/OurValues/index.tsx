340efcda95281613c485069fdd3d5af300ca3bc5
CSCairney <charlescairney@yahoo.co.uk>
2023-11-19 22:48:38 +0000

    fixed some mobile issues and footer

--- Crisp/src/modules/about/components/OurValues/index.tsx
+++ Crisp/src/modules/about/components/OurValues/index.tsx
@@ -2,19 +2,11 @@ import React from "react";
 import "./styles.scss";
 import { OurValuesContent } from "../../../../assets/about/values";
 import Value from "../Value";
-import CompanyValues from "../../../../assets/about/CompanyValues.png";
 
 const OurValues:React.FC = () => {
     return (
         <div className="our-values">
             <div className="our-values__header">
-                <div className="our-values__header--image-container">
-                    <img 
-                        className="our-values__header--image"
-                        src={CompanyValues}
-                        alt="about us header image"
-                    />
-                </div>
                 <div className="our-values__header--content">
                     <h2>Our Values</h2>
                     <h4>At the core of Crisp are values that guide our work:</h4>

a811a9045e06c2ab69d1798097b4a5a023683544
CSCairney <charlescairney@yahoo.co.uk>
2023-11-20 12:49:55 +0000

    Render tests for loader

--- Crisp/src/modules/common/components/Loader/CircularLoader/index.tsx
+++ Crisp/src/modules/common/components/Loader/CircularLoader/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import "./styles.scss";
 
 const CircularLoader: React.FC = () => {
-  return <div className="loader_circle" aria-label="loading..." />;
+  return <div className="loader_circle" aria-label="loading..." data-testid="circular-loader" />;
 };
 
 export default CircularLoader;

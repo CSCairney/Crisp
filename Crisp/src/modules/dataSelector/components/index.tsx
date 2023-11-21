96325b94fd1b90a577f8b6117ff08d7eb7e0810d
CSCairney <charlescairney@yahoo.co.uk>
2023-11-21 11:13:24 +0000

    Login director on the data selector

--- Crisp/src/modules/dataSelector/components/index.tsx
+++ Crisp/src/modules/dataSelector/components/index.tsx
@@ -4,11 +4,15 @@ import { useAppSelector } from "../../../store";
 import { selectDataLayers } from "../store/selectors/dataSelectors";
 import DataLayer from "./dataLayer";
 import DataControls from "./dataControls";
+import { selectAccessToken } from "../../login/store/selectors/login";
+import { Link } from "react-router-dom";
+import { MainRoutes } from "../../../routes";
 
 const DataSelector: React.FC = () => {
   const dataLayerNames = useAppSelector(selectDataLayers);
+  const accessToken = useAppSelector(selectAccessToken);
   
-  return (
+  if (accessToken) return (
     <>
         <div className='data-selector'>
           <ul className="data-selector__list">
@@ -22,6 +26,14 @@ const DataSelector: React.FC = () => {
         </div>
     </>
   );
+
+  return (
+    <div className="data-selector__alternative">
+      <p>No Data?</p>
+      <p>Please login to retrieve</p>
+      <Link to={MainRoutes.LOGIN} className="data-selector__login-button">Login</Link>
+    </div>
+  )
 };
 
 export default DataSelector;

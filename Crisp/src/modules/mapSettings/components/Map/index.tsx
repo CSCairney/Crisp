e9081943497dc520313cdd85ee4aa43aadbf544c
Charles Cairney <charlescairney@yahoo.co.uk>
2023-12-06 16:53:18 +0000

    Working GEO locate

--- Crisp/src/modules/mapSettings/components/Map/index.tsx
+++ Crisp/src/modules/mapSettings/components/Map/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.scss";
 import { MapContainer, TileLayer} from "react-leaflet";
 import MarkersGroup from "../MarkersGroup";
@@ -6,19 +6,28 @@ import { useAppSelector } from "../../../../store";
 import { selectMarkerLayers } from "../../store/selectors/markers";
 import MapControls from "../MapControls";
 import SearchControls from "../SearchControls";
+import { selectCurrentLocation } from "../../../login/store/selectors/login";
+import LocationMarker from "../LocationMarker";
 
 const Map: React.FC = () => {
+  const [center, setCenter] = useState<[number, number]>([55.8611550, -4.2501687]);
   const markerLayers = useAppSelector(selectMarkerLayers);
+  const currentLocation = useAppSelector(selectCurrentLocation);
+
+  if (currentLocation) {
+    setCenter([currentLocation.lat, currentLocation.lng]);
+  }
   return (
     <div className='map-container'>
       <MapContainer 
-        center={[55.8611550, -4.2501687]} 
+        center={center} 
         zoom={7} 
         zoomControl={true}
         scrollWheelZoom={true}
         style={{ width: "100%", height: "100%" }}>
         <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' />
         <MarkersGroup layers={markerLayers}/>
+        <LocationMarker />
         <SearchControls />
         <MapControls />
       </MapContainer>
--- Crisp/src/modules/mapSettings/components/MapControls/index.tsx
+++ Crisp/src/modules/mapSettings/components/MapControls/index.tsx
@@ -1,19 +1,35 @@
-import React from "react";
+import React, { useState} from "react";
 import { FiArrowRightCircle, FiArrowLeftCircle } from "react-icons/fi";
 import { FaLayerGroup } from "react-icons/fa";
 import { BsFilterCircleFill } from "react-icons/bs";
 import { BiStreetView } from "react-icons/bi";
+import { IoMdLocate } from "react-icons/io";
+
 import "./styles.scss";
 import { useAppSelector } from "../../../../store";
 import { selectPanelStatus, selectSidebarStatus } from "../../../common/store/selectors/menu";
 import { useDispatch } from "react-redux";
 import { setPanelOpenStatus, setSidebarOpenStatus } from "../../../common/store/index";
 import { panelTypes } from "../../../common/constants/menu";
+import { useMapEvents } from "react-leaflet";
+import { LatLng } from "leaflet";
+import { toast } from "sonner";
+// import { setCurrentLocation } from "../../../login/store";
 
 const MapControls: React.FC = () => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [location, setLocation] = useState<LatLng>()
   const isSidebarOpen = useAppSelector(selectSidebarStatus);
   const activePanel = useAppSelector(selectPanelStatus);
   const dispatch = useDispatch();
+
+  const map = useMapEvents({
+    locationfound(e) {
+      map.flyTo(e.latlng, 12);
+      setLocation(e.latlng);
+      toast.success("Here you are!");
+    },
+  });
 
   const menuOpenHandler = () => {
     dispatch(setSidebarOpenStatus(true))
@@ -32,6 +48,10 @@ const MapControls: React.FC = () => {
     }
   }
 
+  const locateMeHandler = () => {
+    map.locate(); 
+  }
+
   if (isSidebarOpen) return (
         <div className='map-controls--open'>
           <div className='map-controls__container'>
@@ -43,6 +63,9 @@ const MapControls: React.FC = () => {
             </button>
             <button onClick={() => panelHandler(panelTypes.Views)} className='map-controls__button'>
                 <BiStreetView className='map-controls__icon' />
+            </button>
+            <button onClick={() => locateMeHandler()} className="map-controls__button" id="locate-me-button">
+              <IoMdLocate className='map-controls__icon' />
             </button>
             <button className="map-controls__button" onClick={() => menuCloseHandler()}>
               <FiArrowLeftCircle className='map-controls__icon' />

750388ccda76bf3794ad4986a841134b6cfb715d
Charles Cairney <charlescairney@yahoo.co.uk>
2024-01-25 14:16:17 +0000

    update mock state to follow main store

---
+++ Crisp/tests/mocks/blogState.ts
@@ -0,0 +1,7 @@
+import { blogState } from "../../src/modules/blog/types/blogs";
+
+export const blogMockState: blogState = {
+    isLoading: false,
+    error: "",
+    blogs: []
+    };
--- Crisp/tests/mocks/index.ts
+++ Crisp/tests/mocks/index.ts
@@ -4,11 +4,13 @@ import { userMockState } from "./userState";
 import { mapMockState } from "./mapState";
 import { overlayMockState } from "./overlayState";
 import { dataMockState } from "./dataState";
+import { blogMockState } from "./blogState";
 
 export const mockStoreData: RootState = {
     teamState: teamMockState,
     userState: userMockState,
     mapState: mapMockState,
     overlayState: overlayMockState,
-    dataState: dataMockState
+    dataState: dataMockState,
+    blogState: blogMockState
 };
